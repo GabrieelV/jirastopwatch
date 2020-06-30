67aece98afcd0226dedb64d9dda0ba28b28cc21c
Gabriel <gabrielvie25@outlook.com>
2020-06-29 23:21:49 -0300

    Traduções

---
+++ source/StopWatch/UI/SettingsForm.cs
@@ -0,0 +1,138 @@
+﻿/**************************************************************************
+Copyright 2016 Carsten Gehling
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+**************************************************************************/
+using StopWatch.Logging;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace StopWatch
+{
+
+
+    internal partial class SettingsForm : Form
+    {
+        #region public members
+        public Settings settings { get; private set; }
+        #endregion
+
+
+        #region public methods
+        public SettingsForm(Settings settings)
+        {
+            this.settings = settings;
+
+            InitializeComponent();
+
+            // Mono for MacOSX and Linux do not implement the notifyIcon
+            // so ignore this feature if we are not running on Windows
+            cbMinimizeToTray.Visible = CrossPlatformHelpers.IsWindowsEnvironment();
+
+            tbJiraBaseUrl.Text = this.settings.JiraBaseUrl;
+            tbUsername.Text = settings.Username;
+            tbApiToken.Text = settings.ApiToken;
+
+            cbAlwaysOnTop.Checked = this.settings.AlwaysOnTop;
+            cbMinimizeToTray.Checked = this.settings.MinimizeToTray;
+            cbAllowMultipleTimers.Checked = this.settings.AllowMultipleTimers;
+            cbIncludeProjectName.Checked = this.settings.IncludeProjectName;
+
+            cbSaveTimerState.DisplayMember = "Text";
+            cbSaveTimerState.ValueMember = "Value";
+            cbSaveTimerState.DataSource = new[]
+            {
+                new { Text = "Resetar todos os timers ao fechar", Value = SaveTimerSetting.NoSave },
+                new { Text = "Salvar o tempo atual e pausar os timers ativos", Value = SaveTimerSetting.SavePause },
+                new { Text = "Salvar o tempo atual e continuar os timers ativos", Value = SaveTimerSetting.SaveRunActive }
+            };
+            cbSaveTimerState.SelectedValue = this.settings.SaveTimerState;
+
+            cbPauseOnSessionLock.DisplayMember = "Text";
+            cbPauseOnSessionLock.ValueMember = "Value";
+            cbPauseOnSessionLock.DataSource = new[]
+            {
+                new { Text = "Não pausar", Value = PauseAndResumeSetting.NoPause },
+                new { Text = "Pausar o timer ativo", Value = PauseAndResumeSetting.Pause },
+                new { Text = "Pausar e resumir ao desbloquear", Value = PauseAndResumeSetting.PauseAndResume }
+            };
+            cbPauseOnSessionLock.SelectedValue = this.settings.PauseOnSessionLock;
+
+            cbPostWorklogComment.DisplayMember = "Text";
+            cbPostWorklogComment.ValueMember = "Value";
+            cbPostWorklogComment.DataSource = new[]
+            {
+                new { Text = "Post only as part of worklog", Value = WorklogCommentSetting.WorklogOnly },
+                new { Text = "Post only as a comment", Value = WorklogCommentSetting.CommentOnly },
+                new { Text = "Post as both worklog and comment", Value = WorklogCommentSetting.WorklogAndComment }
+            };
+            cbPostWorklogComment.SelectedValue = this.settings.PostWorklogComment;
+
+            tbStartTransitions.Text = this.settings.StartTransitions;
+
+            cbLoggingEnabbled.Checked = this.settings.LoggingEnabled;
+
+            cbCheckForUpdate.Checked = settings.CheckForUpdate;
+        }
+        #endregion
+
+
+        #region private eventhandlers
+        private void SettingsForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (DialogResult == System.Windows.Forms.DialogResult.OK)
+            {
+                this.settings.JiraBaseUrl = tbJiraBaseUrl.Text;
+                settings.Username = tbUsername.Text;
+                settings.ApiToken = tbApiToken.Text;
+
+                this.settings.AlwaysOnTop = cbAlwaysOnTop.Checked;
+                this.settings.MinimizeToTray = cbMinimizeToTray.Checked;
+                this.settings.AllowMultipleTimers = cbAllowMultipleTimers.Checked;
+                this.settings.IncludeProjectName = cbIncludeProjectName.Checked;
+
+                this.settings.SaveTimerState = (SaveTimerSetting)cbSaveTimerState.SelectedValue;
+                this.settings.PauseOnSessionLock = (PauseAndResumeSetting)cbPauseOnSessionLock.SelectedValue;
+                this.settings.PostWorklogComment = (WorklogCommentSetting)cbPostWorklogComment.SelectedValue;
+
+                this.settings.StartTransitions = tbStartTransitions.Text;
+
+                this.settings.LoggingEnabled = cbLoggingEnabbled.Checked;
+                settings.CheckForUpdate = cbCheckForUpdate.Checked;
+            }
+        }
+
+
+        private void btnAbout_Click(object sender, System.EventArgs e)
+        {
+            using (var aboutForm = new AboutForm())
+            {
+                aboutForm.ShowDialog();
+            }
+        }
+        #endregion
+
+        private void lblOpenLogFolder_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start(Path.GetDirectoryName(Logger.Instance.LogfilePath));
+        }
+
+        /*
+        private void lblOpenAPITokensPage_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://id.atlassian.com/manage/api-tokens");
+        }
+        */
+    }
+}
---
+++ source/StopWatchTest/JiraApiRequesterTest.cs
@@ -0,0 +1,108 @@
+﻿namespace StopWatchTest
+{
+    using Moq;
+    using NUnit.Framework;
+    using RestSharp;
+    using StopWatch;
+    using System.Linq;
+    using System.Net;
+
+    internal class TestPocoClass
+    {
+        public string foo { get; set; }
+        public string bar { get; set; }
+    }
+
+    [TestFixture]
+    public class JiraApiRequesterTest
+    {
+        private Mock<IRestClient> clientMock;
+        private Mock<IRestClientFactory> clientFactoryMock;
+
+        private Mock<IJiraApiRequestFactory> jiraApiRequestFactoryMock;
+
+        private JiraApiRequester jiraApiRequester;
+
+        [SetUp]
+        public void Setup()
+        {
+            clientMock = new Mock<IRestClient>();
+
+            clientFactoryMock = new Mock<IRestClientFactory>();
+            clientFactoryMock.Setup(c => c.Create(It.IsAny<bool>())).Returns(clientMock.Object);
+
+            jiraApiRequestFactoryMock = new Mock<IJiraApiRequestFactory>();
+
+            jiraApiRequester = new JiraApiRequester(clientFactoryMock.Object, jiraApiRequestFactoryMock.Object);
+        }
+
+        private static IRestResponse<TestPocoClass> TestAuth(IRestRequest requestMock, string valid_username, string valid_apitoken)
+        {
+            var authParam = requestMock.Parameters.FirstOrDefault(p => p.Type == ParameterType.HttpHeader && p.Name == "Authorization");
+            const string prefix = "Basic ";
+            if (authParam != null)
+            {
+                if (authParam.Value is string && ((string)authParam.Value).StartsWith(prefix))
+                {
+                    var base64 = ((string)authParam.Value).Substring(prefix.Length);
+                    try
+                    {
+                        string authString = System.Text.Encoding.UTF8.GetString(System.Convert.FromBase64String(base64));
+                        var comps = authString.Split(':');
+                        if (comps.Length == 2 && comps[0] == valid_username && comps[1] == valid_apitoken)
+                        {
+                            return new RestResponse<TestPocoClass>()
+                            {
+                                StatusCode = HttpStatusCode.OK,
+                                Data = new TestPocoClass() { foo = "foo", bar = "bar" },
+                            };
+                        }
+                    }
+                    catch (System.Exception)
+                    { }
+                }
+            }
+            return new RestResponse<TestPocoClass>()
+            {
+                StatusCode = HttpStatusCode.Unauthorized
+            };
+        }
+
+        [Test, Description("DoAuthenticatedRequest: with correct credentials return data without error message")]
+        public void DoAuthenticatedRequest_WithValidCredentials()
+        {
+            var valid_username = "validusername";
+            var valid_apitoken = "validapitoken";
+
+            var requestMock = new RestRequest();
+
+            clientMock.Setup(c => c.Execute<TestPocoClass>(It.IsAny<IRestRequest>())).Returns(() => TestAuth(requestMock, valid_username, valid_apitoken));
+
+            jiraApiRequester.SetAuthentication(valid_username, valid_apitoken);
+
+            var response = jiraApiRequester.DoAuthenticatedRequest<TestPocoClass>(requestMock);
+
+            Assert.NotNull(response);
+            Assert.IsEmpty(jiraApiRequester.ErrorMessage);
+        }
+
+        [Test, Description("DoAuthenticatedRequest: with wrong credentials it throws an exception")]
+        public void DoAuthenticatedRequest_WithInvalidCredentials()
+        {
+            var valid_username = "validusername";
+            var valid_apitoken = "validapitoken";
+
+            var requestMock = new RestRequest();
+
+            clientMock.Setup(c => c.Execute<TestPocoClass>(It.IsAny<IRestRequest>())).Returns(() => TestAuth(requestMock, valid_username, valid_apitoken));
+
+            jiraApiRequester.SetAuthentication("invalidUsername", "invalidApiToken");
+
+            Assert.Throws<RequestDeniedException>(() =>
+            {
+                var response = jiraApiRequester.DoAuthenticatedRequest<TestPocoClass>(requestMock);
+            });
+        }
+
+    }
+}
